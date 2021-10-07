fcc84946a8e0e7cf61a3ea08b6b02ac99254b30b
Noah <noq@noq.local>
2021-10-07 09:15:52 -0600

    added new list functionality

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
-let listsDisplay = document.getElementById('lists-display');
-let listContentsDisplay = document.getElementById('lists-items-display');
 
+//
+// List class definition
+//
 
+// Child of List
 class ListItem {
-	constructor(content = "") {
+	constructor(id, content = "") {
+		this.id = id;
 		this.content = content;
 		this.isChecked = false;
 	}
@@ -13,62 +16,107 @@ class ListItem {
 	}
 }
 
-
-/*
-	note: functions that edit DOM should be outside of objects
-*/
-
-function renderList() {
-
-}
-
+// List contains ListItems
 class List {
-	constructor(id, name = "New List") {
+	constructor(name = "New List", id = List.generateID()) {
 		this.name = name;			// name is mutable
 		this.id = id;				// ID must be unique
 		this.contents = [];			// stores ListItems
 		this.isActive = false;
+		this.currentChildID = 0;
 	}
-	addListItem(text) {
-		this.contents.push(new ListItem(text));
-		this.render();
+	addListItem(content) {
+		let listItem = new ListItem(this.generateChildID(), content);
+		this.contents.push(listItem);
+		renderListItem(listItem);
 	}
-	render() {						// draws all ListItems in listContentsDisplay element
-		// for (let listItem of this.contents) {
-		// 	console.log(listItem);
-		// }
+	render() {						// Clears list item display and draws contents
+
 	}
 	activate() {
 		this.isActive = true;
 		List.active = this;
 	}
 	delete() {						// allows List instance to delete itself
-
+		deleteList(this);
 	}
-
-
-	static renderLists() {			// draws all Lists in listsDisplay element
-		// for (let list of List.lists) {
-		// 	console.log(list);
-		// }
+	generateChildID() {
+		return `${this.id}-${++this.currentChildID}`;
 	}
 
 	static currentID = 0;
 	static generateID() {
-		return `list-${++List.currentID}`;
-	}
-	static newList() {
-		List.lists.push(new List("id"));
-	}
-	static activate(id) {
-		// List.lists
+		return `list-${List.currentID++}`;
 	}
 	static active;					// stores active List
-	static lists = [];				// stores all List instances
 
 }
 
 
-let list1 = new List("list-1", "list1");
+//
+//	List functionality
+//
+
+let listsDisplay = document.getElementById('lists-display');
+let listContentsDisplay = document.getElementById('list-items-display');
+let lists = [];
+
+
+function getListItemHTML(listItem) {
+	let element = `
+		<div class='list-item row-center' id=${listItem.id}>
+			<div class='left row-center'>
+				<button class='btn checkbox' onclick='checkSelf(${listItem.id});'>
+					<i class='fas fa-check'></i>
+				</button>
+				<div class='text'>${listItem.content}</div>
+			</div>
+			<div class='right row-center'>
+				<div class='tools-container row-center'>
+					<button class='btn trash' onclick='deleteSelf(${listItem.id});'>
+						<i class='fas fa-times'></i>
+					</button>
+				</div>
+			</div>
+		</div>`;
+	return element;
+}
+function renderListItem(listItem) {				// appends a ListItem to display
+	listContentsDisplay.innerHTML += getListItemHTML(listItem);
+}
+
+
+function getListHTML(list = null) {
+
+}
+function renderListsMenu(listsArray = lists) {	// appends a List to display
+	for (let list of lists) {
+		console.log(list);
+	}
+}
+
+
+function renderList(list) {
+	listContentsDisplay.innerHTML = "";			// clears listContentsDisplay
+	for (let listItem in list.contents) {		// writes contents in display
+		renderListItem(listItem);
+	}
+}
+
+function deleteList(list) {
+	document.getElementById(list.id).style = 'display: none';
+	listContentsDisplay.innerHTML = "";
+}
+
+function clearChecked(list) {
+
+}
+
+let list1 = new List("list1");
 list1.activate();
 
+List.active.addListItem("text");
+List.active.addListItem("text");
+List.active.addListItem("text");
+
+List.active.render();
